2b6fd530e97e7a285824a29819d1a66a17496f82
DUO <imshengong@hotmail.com>
2018-03-27 11:25:47 +0800

    ddpg_ed

--- Morvan_Robot_Arm/DDPG.py
+++ Morvan_Robot_Arm/DDPG.py
@@ -23,7 +23,7 @@ ENV_NAME = 'Pendulum-v0'
 REPLACEMENT = [
     dict(name='soft', tau=0.01),
     dict(name='hard', rep_iter_a=600, rep_iter_c=500)
-][0]
+][1]
 
 
 class Actor(object):
@@ -36,7 +36,7 @@ class Actor(object):
 
         self.t_replace_counter = 0
 
-        with tf.name_scope('Actor'):
+        with tf.variable_scope('Actor'):
             # input s, out put a
             self.a = self._build_net(S, scope='eval_net', trainable=True)
             # input s_, output a, get a_ for critic
@@ -53,7 +53,19 @@ class Actor(object):
                                  for t, e in zip(self.t_params, self.e_params)]
 
     def _build_net(self, s, scope, trainable):
-        with tf.name_scope(scope):
+        # with tf.variable_scope(scope):
+        #     init_w = tf.random_normal_initializer(0., 0.3)
+        #     init_b = tf.constant_initializer(0.1)
+        #     net = tf.layers.dense(s, 30, activation=tf.nn.relu,
+        #                           kernel_initializer=init_w, bias_initializer=init_b, name='l1',
+        #                           trainable=trainable)
+        #     with tf.variable_scope('a'):
+        #         actions = tf.layers.dense(net, self.a_dim, activation=tf.nn.tanh, kernel_initializer=init_w,
+        #                                   bias_initializer=init_b, name='a', trainable=trainable)
+        #         scaled_a = tf.multiply(actions, self.action_bound, name='scaled_a')  # Scale output to -action_bound to action_bound
+        # return scaled_a
+
+        with tf.variable_scope(scope):
             init_w = tf.random_normal_initializer(mean=0., stddev=0.3)
             init_b = tf.constant_initializer(0.1)
             net = tf.layers.dense(
@@ -66,7 +78,7 @@ class Actor(object):
                 trainable=trainable
             )
 
-            with tf.name_scope('a'):
+            with tf.variable_scope('a'):
                 actions = tf.layers.dense(
                     inputs=net,
                     units=self.a_dim,
@@ -98,32 +110,123 @@ class Actor(object):
 
     def add_grad_to_graph(self, a_grads):
         with tf.variable_scope('policy_grads'):
+            # ys = policy;
+            # xs = policy's parameters;
+            # a_grads = the gradients of the policy to get more Q
+            # tf.gradients will calculate dys/dxs with a initial gradients for ys, so this is dq/da * da/dparams
             self.policy_grads = tf.gradients(ys=self.a, xs=self.e_params, grad_ys=a_grads)
+
         with tf.variable_scope('A_train'):
-            opt = tf.train.AdamOptimizer(-self.lr) # -lr for ascent policy
+            opt = tf.train.AdamOptimizer(-self.lr)  # (- learning rate) for ascent policy
             self.train_op = opt.apply_gradients(zip(self.policy_grads, self.e_params))
+        # with tf.variable_scope('policy_grads'):
+        #     self.policy_grads = tf.gradients(ys=self.a, xs=self.e_params, grad_ys=a_grads)
+        # with tf.variable_scope('A_train'):
+        #     opt = tf.train.AdamOptimizer(-self.lr) # -lr for ascent policy
+        #     self.train_op = opt.apply_gradients(zip(self.policy_grads, self.e_params))
 
 
 class Critic(object):
     def __init__(self, sess, state_dim, action_dim, learning_rate, gamma, replacement, a, a_):
-        pass
+        self.sess = sess
+        self.s_dim = state_dim
+        self.a_dim = action_dim
+        self.lr = learning_rate
+        self.gamma = gamma
+        self.replacement = replacement
 
-    def _build_net(self):
-        pass
+        with tf.variable_scope('Critic'):
+            self.a = a
+            self.q = self._build_net(S, self.a, 'eval_net', trainable=True)
+
+            self.q_ = self._build_net(S_, a_, 'target_net', trainable=False)
+
+            self.e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/eval_net')
+            self.t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/target_net')
+
+        with tf.variable_scope('target_q'):
+            self.target_q = R + self.gamma * self.q_
+
+        with tf.variable_scope('td_error'):
+            self.loss = tf.reduce_mean(tf.squared_difference(self.target_q, self.q))
+
+        with tf.variable_scope('C_train'):
+            self.train_op = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
+
+        with tf.variable_scope('a_grad'):
+            self.a_grads = tf.gradients(self.q, a)[0]
+
+        if self.replacement['name'] == 'hard':
+            self.t_replace_counter = 0
+            self.hard_replacement = [tf.assign(t, e) for t, e in zip(self.t_params, self.e_params)]
+        else:
+            self.soft_replacement = [tf.assign(t, (1-self.replacement['tau'] * t + self.replacement['tau'] * e))
+                                     for t, e in zip(self.t_params, self.e_params)]
+
+    def _build_net(self, s, a, scope, trainable):
+        # with tf.variable_scope(scope):
+        #     init_w = tf.random_normal_initializer(0., 0.1)
+        #     init_b = tf.constant_initializer(0.1)
+        #
+        #     with tf.variable_scope('l1'):
+        #         n_l1 = 30
+        #         w1_s = tf.get_variable('w1_s', [self.s_dim, n_l1], initializer=init_w, trainable=trainable)
+        #         w1_a = tf.get_variable('w1_a', [self.a_dim, n_l1], initializer=init_w, trainable=trainable)
+        #         b1 = tf.get_variable('b1', [1, n_l1], initializer=init_b, trainable=trainable)
+        #         net = tf.nn.relu(tf.matmul(s, w1_s) + tf.matmul(a, w1_a) + b1)
+        #
+        #     with tf.variable_scope('q'):
+        #         q = tf.layers.dense(net, 1, kernel_initializer=init_w, bias_initializer=init_b, trainable=trainable)   # Q(s,a)
+        # return q
+        with tf.variable_scope(scope):
+            init_w = tf.random_normal_initializer(mean=0., stddev=0.1)
+            init_b = tf.constant_initializer(0.1)
+
+            with tf.variable_scope('l1'):
+                n_l1 = 30
+                w1_s = tf.get_variable('w1_s', [self.s_dim, n_l1], initializer=init_w, trainable=trainable)
+                w1_a = tf.get_variable('w1_a', [self.a_dim, n_l1], initializer=init_w, trainable=trainable)
+
+                b1 = tf.get_variable('b1', [1, n_l1], initializer=init_b, trainable=trainable)
+                net = tf.nn.relu(tf.matmul(s, w1_s) + tf.matmul(a, w1_a) + b1)
+
+            with tf.variable_scope('q'):
+                q = tf.layers.dense(
+                    inputs=net,
+                    units=1,
+                    kernel_initializer=init_w,
+                    bias_initializer=init_b,
+                    trainable=trainable
+                )
+        return q
 
     def learn(self, s, a, r, s_):
-        pass
+        self.sess.run(self.train_op, feed_dict={S: s, self.a: a, R: r, S_: s_})
+        if self.replacement['name'] == 'soft':
+            self.sess.run(self.soft_replacement)
+
+        else:
+            if self.t_replace_counter % self.replacement['rep_iter_c'] == 0:
+                self.sess.run(self.hard_replacement)
+            self.t_replace_counter += 1
 
 
 class Memory(object):
     def __init__(self, capactiy, dims):
-        pass
+        self.capacity = capactiy
+        self.data = np.zeros((capactiy, dims))
+        self.pointer = 0
 
     def store_transition(self, s, a, r, s_):
-        pass
+        transition = np.hstack((s, a, [r], s_))
+        index = self.pointer % self.capacity
+        self.data[index, :] = transition
+        self.pointer += 1
 
     def sample(self, n):
-        pass
+        assert self.pointer >= self.capacity, 'Memory has not been fulfilled'
+        indices = np.random.choice(self.capacity, size=n)
+        return self.data[indices, :]
 
 
 env = gym.make(ENV_NAME)
@@ -147,3 +250,52 @@ with tf.name_scope('S_'):
 sess = tf.Session()
 actor = Actor(sess, action_dim, action_bound, LR_A, REPLACEMENT)
 critic = Critic(sess, state_dim, action_dim, LR_C, GAMMA, REPLACEMENT, actor.a, actor.a_)
+actor.add_grad_to_graph(critic.a_grads)
+
+sess.run(tf.global_variables_initializer())
+
+M = Memory(MEMORY_CAPACITY, dims=2*state_dim+action_dim+1)
+
+if OUTPUT_GRAPH:
+    tf.summary.FileWriter('DDPG/logs/', sess.graph)
+
+var = 3
+
+t1 = time.time()
+
+for i in range(MAX_EPISODE):
+    s = env.reset()
+    ep_reward = 0
+
+    for j in range(MAX_EP_STEP):
+        if RENDER:
+            env.render()
+        a = actor.choose_action(s)
+        a = np.clip(np.random.normal(a, var), -2, 2)
+
+        s_, r, done, info = env.step(a)
+
+        M.store_transition(s, a, r/10, s_)
+
+        if M.pointer > MEMORY_CAPACITY:
+            var *= 0.995
+            b_M = M.sample(BATCH_SIZE)
+            b_s = b_M[:, :state_dim]
+            b_a = b_M[:, state_dim:state_dim+action_dim]
+            b_r = b_M[:, -state_dim-1:-state_dim]
+            b_s_ = b_M[:, -state_dim:]
+
+            critic.learn(b_s, b_a, b_r, b_s_)
+            actor.learn(b_s)
+
+        s = s_
+        ep_reward += r
+
+        if j == MAX_EP_STEP - 1:
+            print('Episode:', i, ' Reward: %i' % int(ep_reward), 'Explore: %.2f' % var, )
+            if ep_reward > -300:
+                RENDER = True
+            break
+
+print('Running time: ', time.time()-t1)
+
